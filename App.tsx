21bb076de40baa5b7e3ea21beb0abe01eea26949
haipv01 <haipv@bsscommerce.com>
2024-01-08 23:42:18 +0700

    update animation functions

--- App.tsx
+++ App.tsx
@@ -1,23 +1,31 @@
 import { Button, View } from 'react-native';
-import Animated, { useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+import WithDecayComponent from './src/animated/functions/withDecay';
+import WithSequence from './src/animated/functions/withSequence';
+import WithSpringComponent from './src/animated/functions/withSpring';
+import WithTimingComponent from './src/animated/functions/withTiming';
+const paths = {
+  firstAnimation: 'FIRST_ANIMATION',
+  withDecay: 'WITH_DECAY'
+}
+const listFeatures = [
+  {
+    name: 'First animation',
+    path: paths.firstAnimation
+  },
+  {
+    name: 'With decay',
+    path: paths.withDecay
+  },
+]
 
 export default function App() {
-  const width = useSharedValue(100);
-
-  const handlePress = () => {
-    width.value = withSpring(width.value + 50);
-  };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Animated.View
-        style={{
-          width,
-          height: 100,
-          backgroundColor: 'violet',
-        }}
-      />
-      <Button onPress={handlePress} title="Click me" />
+      {/* <WithDecayComponent /> */}
+      {/* <WithSequence /> */}
+      {/* <WithSpringComponent /> */}
+      <WithTimingComponent />
     </View>
   );
 }
---
+++ src/firstAnimation/index.tsx
@@ -0,0 +1,23 @@
+import { Button, View } from 'react-native';
+import Animated, { useSharedValue, withSpring, withTiming } from 'react-native-reanimated';
+
+export default function FirstAnimation() {
+  const width = useSharedValue(100);
+
+  const handlePress = () => {
+    width.value = withSpring(width.value + 50);
+  };
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Animated.View
+        style={{
+          width,
+          height: 100,
+          backgroundColor: 'violet',
+        }}
+      />
+      <Button onPress={handlePress} title="Click me" />
+    </View>
+  );
+}
